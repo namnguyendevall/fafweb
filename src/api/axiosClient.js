660516640c04540962a383fed8564e6df9793ee5
namnguyendevall <binbibna@gmail.com>
2026-03-27 17:08:22 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
   timeout: 10000,
 });
 
